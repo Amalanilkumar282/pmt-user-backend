51086734976bd8dee381470d5bc52cb7848d520f
NadimNaisam <nadimvc@gmail.com>
2025-10-28 10:41:09 +0530

    made API to get all labels

--- BACKEND_CQRS.Api/Controllers/LabelController.cs
+++ BACKEND_CQRS.Api/Controllers/LabelController.cs
@@ -1,7 +1,10 @@
 using BACKEND_CQRS.Application.Command;
+using BACKEND_CQRS.Application.Query;
+using BACKEND_CQRS.Application.Dto;
 using BACKEND_CQRS.Application.Wrapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BACKEND_CQRS.Api.Controllers
@@ -20,6 +23,13 @@ namespace BACKEND_CQRS.Api.Controllers
         public async Task<ApiResponse<int>> CreateLabel([FromBody] CreateLabelCommand command)
         {
             var result = await _mediator.Send(command);
+            return result;
+        }
+
+        [HttpGet]
+        public async Task<ApiResponse<List<LabelDto>>> GetAllLabels()
+        {
+            var result = await _mediator.Send(new GetAllLabelsQuery());
             return result;
         }
     }
