1d6ab0c6601bc572d20e5528c0c00e76372512bd
Sharath Shony <sharath.shony@experionglobal.local>
2025-10-29 15:15:52 +0530

    GetSprintsByProjectId done.

--- BACKEND_CQRS.Api/Controllers/SprintController.cs
+++ BACKEND_CQRS.Api/Controllers/SprintController.cs
@@ -1,17 +1,20 @@
 using BACKEND_CQRS.Application.Command;
 using BACKEND_CQRS.Application.Dto;
+using BACKEND_CQRS.Application.Query.Sprints;
 using BACKEND_CQRS.Application.Wrapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace BACKEND_CQRS.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/sprints")]
     [ApiController]
     public class SprintController : ControllerBase
     {
         private readonly IMediator _mediator;
+        
         public SprintController(IMediator mediator)
         {
             _mediator = mediator;
@@ -22,6 +25,20 @@ namespace BACKEND_CQRS.Api.Controllers
         {
             var newSprint = await _mediator.Send(command);
             return newSprint;
+        }
+
+        [HttpGet("project/{projectId}")]
+        public async Task<IActionResult> GetSprintsByProjectId(Guid projectId)
+        {
+            var query = new GetSprintsByProjectIdQuery(projectId);
+            var result = await _mediator.Send(query);
+
+            if (result.Status != 200)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
         }
     }
 }
