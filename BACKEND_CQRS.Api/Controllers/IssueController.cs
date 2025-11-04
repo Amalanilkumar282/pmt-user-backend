042cead6ebde66e6499d459a72f2a01271f6e40b
Kavya17012003 <lbt21cs056@gmail.com>
2025-11-04 15:12:03 +0530

    Added API

--- BACKEND_CQRS.Api/Controllers/IssueController.cs
+++ BACKEND_CQRS.Api/Controllers/IssueController.cs
@@ -155,83 +155,92 @@ namespace BACKEND_CQRS.Api.Controllers
 
             return Ok(response);
         }
-            [HttpGet("project/{projectId}/statuses")]
-            public async Task<ApiResponse<List<StatusDto>>> GetStatusesByProject([FromRoute] Guid projectId)
-            {
-                var query = new GetStatusesByProjectQuery(projectId);
-                return await _mediator.Send(query);
-            }
+        [HttpGet("project/{projectId}/statuses")]
+        public async Task<ApiResponse<List<StatusDto>>> GetStatusesByProject([FromRoute] Guid projectId)
+        {
+            var query = new GetStatusesByProjectQuery(projectId);
+            return await _mediator.Send(query);
+        }
 
-            #region Issue Comments
+        #region Issue Comments
 
-            /// <summary>
-            /// Create a new comment for an issue
-            /// </summary>
-            [HttpPost("{issueId}/comments")]
-            public async Task<ApiResponse<CreateIssueCommentDto>> CreateComment(
-                [FromRoute] Guid issueId,
-                [FromBody] CreateIssueCommentCommand command)
-            {
-                command.IssueId = issueId;
-                var result = await _mediator.Send(command);
-                return result;
-            }
+        /// <summary>
+        /// Create a new comment for an issue
+        /// </summary>
+        [HttpPost("{issueId}/comments")]
+        public async Task<ApiResponse<CreateIssueCommentDto>> CreateComment(
+            [FromRoute] Guid issueId,
+            [FromBody] CreateIssueCommentCommand command)
+        {
+            command.IssueId = issueId;
+            var result = await _mediator.Send(command);
+            return result;
+        }
 
-            /// <summary>
-            /// Get all comments for a specific issue
-            /// </summary>
-            [HttpGet("{issueId}/comments")]
-            public async Task<ApiResponse<List<IssueCommentDto>>> GetCommentsByIssueId([FromRoute] Guid issueId)
-            {
-                var query = new GetCommentsByIssueIdQuery(issueId);
-                var result = await _mediator.Send(query);
-                return result;
-            }
+        /// <summary>
+        /// Get all comments for a specific issue
+        /// </summary>
+        [HttpGet("{issueId}/comments")]
+        public async Task<ApiResponse<List<IssueCommentDto>>> GetCommentsByIssueId([FromRoute] Guid issueId)
+        {
+            var query = new GetCommentsByIssueIdQuery(issueId);
+            var result = await _mediator.Send(query);
+            return result;
+        }
 
-            /// <summary>
-            /// Get a specific comment by ID
-            /// </summary>
-            [HttpGet("comments/{commentId}")]
-            public async Task<ApiResponse<IssueCommentDto>> GetCommentById([FromRoute] Guid commentId)
-            {
-                var query = new GetCommentByIdQuery(commentId);
-                var result = await _mediator.Send(query);
-                return result;
-            }
+        /// <summary>
+        /// Get a specific comment by ID
+        /// </summary>
+        [HttpGet("comments/{commentId}")]
+        public async Task<ApiResponse<IssueCommentDto>> GetCommentById([FromRoute] Guid commentId)
+        {
+            var query = new GetCommentByIdQuery(commentId);
+            var result = await _mediator.Send(query);
+            return result;
+        }
 
-            /// <summary>
-            /// Update an existing comment
-            /// </summary>
-            [HttpPut("comments/{commentId}")]
-            public async Task<ApiResponse<Guid>> UpdateComment(
-                [FromRoute] Guid commentId,
-                [FromBody] UpdateIssueCommentCommand command)
-            {
-                command.Id = commentId;
-                var result = await _mediator.Send(command);
-                return result;
-            }
+        /// <summary>
+        /// Update an existing comment
+        /// </summary>
+        [HttpPut("comments/{commentId}")]
+        public async Task<ApiResponse<Guid>> UpdateComment(
+            [FromRoute] Guid commentId,
+            [FromBody] UpdateIssueCommentCommand command)
+        {
+            command.Id = commentId;
+            var result = await _mediator.Send(command);
+            return result;
+        }
 
-            /// <summary>
-            /// Delete a comment
-            /// </summary>
-            [HttpDelete("comments/{commentId}")]
-            public async Task<ApiResponse<Guid>> DeleteComment([FromRoute] Guid commentId)
-            {
-                var command = new DeleteIssueCommentCommand(commentId);
-                var result = await _mediator.Send(command);
-                return result;
-            }
+        /// <summary>
+        /// Delete a comment
+        /// </summary>
+        [HttpDelete("comments/{commentId}")]
+        public async Task<ApiResponse<Guid>> DeleteComment([FromRoute] Guid commentId)
+        {
+            var command = new DeleteIssueCommentCommand(commentId);
+            var result = await _mediator.Send(command);
+            return result;
+        }
 
 
         [HttpGet("project/{projectId}/activity-summary")]
-        public async Task<ApiResponse<Dictionary<string, int>>> GetIssueActivitySummary([FromRoute] Guid projectId)
+        public async Task<ApiResponse<Dictionary<string, int>>> GetIssueActivitySummaryByProjectId([FromRoute] Guid projectId)
         {
             var query = new GetIssueActivitySummaryByProjectQuery(projectId);
             return await _mediator.Send(query);
         }
 
-        #endregion
+        [HttpGet("project/{projectId}/sprint/{sprintId}/activity-summary")]
+        public async Task<ApiResponse<Dictionary<string, int>>> GetIssueActivitySummaryBySprintId(
+         [FromRoute] Guid projectId,
+        [FromRoute] Guid sprintId)
+        {
+            var query = new GetIssueActivitySummaryBysprintIdQuery(projectId, sprintId);
+            return await _mediator.Send(query);
+        }
     }
-    }
+    #endregion
+
+}
 
---
+++ BACKEND_CQRS.Application/Query/Issues/GetIssueActivitySummaryBySprintIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using BACKEND_CQRS.Application.Wrapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BACKEND_CQRS.Application.Query.Issues
+{
+    public class GetIssueActivitySummaryBysprintIdQuery : IRequest<ApiResponse<Dictionary<string, int>>>
+    {
+        public Guid ProjectId { get; set; }
+        public Guid? SprintId { get; set; }   // Optional — if not provided, calculate for the entire project
+
+        public GetIssueActivitySummaryBysprintIdQuery(Guid projectId, Guid? sprintId = null)
+        {
+            ProjectId = projectId;
+            SprintId = sprintId;
+        }
+    }
+}
